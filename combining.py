d583f5dd99952e78483edb4a9fad22f12870aa44
Arihant Rastogi <143735797+ArihantRastogi@users.noreply.github.com>
2025-02-20 16:22:14 +0530

    Add files via upload

---
+++ combining.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+all_data = []
+
+for year in range(2015, 2024):
+    file_path = f'data/WHR_{year}.csv'
+    df = pd.read_csv(file_path)
+    df['year'] = year
+    all_data.append(df)
+
+combined_df = pd.concat(all_data, ignore_index=True)
+combined_df.sort_values(['country', 'year'], inplace=True)
+combined_df.to_csv('final.csv', index=False)
